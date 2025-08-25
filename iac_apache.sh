10a4227f6e54acd12b7b5969a76385ad61e493fd
daniel-comp-dev <daniel.dev.tec@outlook.com>
2025-08-25 17:05:26 -0300

    Enviando projeto.

---
+++ iac_apache.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "===================================="
+echo "Subindo um servidor web."
+echo "===================================="
+echo ""
+echo "- Atualizando o servidor."
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+
+cp -r * /var/www/html/
+
